1dea75a70e942fd1a491f4a977d5b62f54e7117f
riccardobattiato <rbattiato@outlook.it>
2024-07-27 14:11:28 +0200

    chore: remove rn bare template example app

--- app-example/(app)/(tabs)/profile.tsx
+++
@@ -1,5 +0,0 @@
-import Account from "@/components/Account";
-
-export default function Page() {
-  return <Account />;
-}
--- app-example/(app)/test.tsx
+++
@@ -1,7 +0,0 @@
-import { useLocalSearchParams } from "expo-router";
-
-import { Text } from "react-native";
-
-export default function Page() {
-  return <Text>Test Page 12</Text>;
-}
